1cf446a614f0a5036d4b42c05b184fa5f50b914a
Spencer Leibow <spencerleibow@spencers-air.iceenterprise.com>
2018-02-26 14:12:22 -0700

    Initial commit of main application file.

---
+++ music_collection.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const artists = [
+	{name: "Acuostic Alchemy"},
+	{name: "Fourplay"},
+	{name: "Hiroshima"},
+	{name: "Pat Metheny"},
+	{name: "Rippingtons"},
+	{name: "Spyro Gyra"},
+	{name: "Yellowjackets"}
+];
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(artists, null, 2));
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
