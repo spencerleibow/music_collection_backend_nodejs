9292cf993ac9af75ebda8fb4f3f6a9934636a4c5
spencerleibow <spencerleibow@10.0.1.11>
2018-04-21 15:00:11 -0700

    Get data from DB.

--- music_collection.js
+++ music_collection.js
@@ -1,64 +1,55 @@
-const http = require('http');
-const url = require('url')
+const express = require("express"); 
+const mysql = require('mysql');
 
-const Router = require('routes');
-const router = Router();
-
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const artists = [
-	{id: 1, name: "Acuostic Alchemy"},
-	{id: 2, name: "Craig Chaquico"},
-	{id: 3, name: "David Benoit"},
-	{id: 4, name: "Fourplay"},
-	{id: 5, name: "Hiroshima"},
-	{id: 6, name: "Pat Metheny Group"},
-	{id: 7, name: "Rippingtons"},
-	{id: 8, name: "Spyro Gyra"},
-	{id: 9, name: "Yellowjackets"}
-];
-
-const albums = [
-	{id: 1, artist_id: 1, name: "Blue Chip"},
-	{id: 2, artist_id: 1, name: "Back On The Case"},
-	{id: 3, artist_id: 1, name: "Aart"},
-	{id: 4, artist_id: 4, name: "Elixir"},
-	{id: 5, artist_id: 4, name: "Heartfelt"},
-	{id: 6, artist_id: 4, name: "X"},
-	{id: 7, artist_id: 6, name: "Ametican Garage"},
-	{id: 8, artist_id: 6, name: "First Circle"},
-	{id: 9, artist_id: 8, name: "Morning Dance"},
-	{id: 10, artist_id: 8, name: "Breakout"}
-];
-
-router.addRoute("/artists", getArtists);
-router.addRoute("/albums", getAlbums);
-router.addRoute("*", noop);
-
-function getArtists(req, res, path) {
-	res.write(JSON.stringify(artists, null, 2));
+function getDBConnection() {
+	return mysql.createConnection({
+		  host: "localhost",
+		  user: "spencerleibow",
+		  password: "sirspence",
+		  database: "MusicCollection"
+	});
 }
 
-function getAlbums(req, res, path) {
-	res.write(JSON.stringify(albums, null, 2));
-}
-
-function noop() {}
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  
-  var path = url.parse(req.url).pathname;
-  console.log("path=" + path);
-  var match = router.match(path);
-  match.fn(req, res, match);
-  res.end();
+app.get("/artists", function(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+	var con = getDBConnection();
+	con.connect(function(err) {
+		if (err) throw err;
+		con.query("SELECT * from artist", function (err, result) {
+		    if (err) throw err;
+		    console.log("artists=" + JSON.stringify(result, null, 2));
+			res.json(result, null, 2);
+		  });
+	});
 });
 
-server.listen(port, hostname, () => {
+app.get("/albums", function(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+	var con = getDBConnection();
+	var artist_id = req.query.artist_id;
+	con.connect(function(err) {
+		if (err) throw err;
+		var sql;
+		if (	artist_id == null) {
+			sql = "SELECT a.id, a.name name, b.name artist_name, a.year from album a" +
+			" INNER JOIN artist b ON a.artist_id = b.id";
+		} else { 
+			sql = "SELECT a.id, a.name, a.year from album a" +
+			" WHERE a.artist_id = " + artist_id;
+		}
+		con.query(sql, function (err, result) {
+			if (err) throw err;
+			console.log("artists=" + JSON.stringify(result, null, 2));
+			res.json(result, null, 2);
+		});
+	});
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
